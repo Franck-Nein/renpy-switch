e369d30e366ad1ef94e8de38eb3a495b1c8fe2a9
Franck-Nein <franco600@live.com.ar>
2021-05-14 06:45:18 -0300

    a

---
+++ convert-game-low.sh
@@ -0,0 +1,7 @@
+pushd romfs/Contents/game/
+find . -name "*.png" | parallel -eta cwebp {} -o {.}.png
+find . -name "*.jpg" | parallel -eta cwebp {} -o {.}.jpg
+find . -name "*.ogg" | parallel -eta oggdec {}
+find . -name "*.wav" | parallel -eta opusenc {} --comp 10 {.}.ogg
+find . -name "*.wav" | parallel -eta rm {}
+popd
--- setup-docker.bash
+++ setup-docker.bash
@@ -1,4 +1,4 @@
-
+docker rm switchdev
 docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20210306 tail -f /dev/null
 curl -LOC - https://github.com/uyjulian/pacman-packages/releases/download/v2.2.3-1-pkgbuild-helpers/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
 docker exec switchdev /bin/bash -c 'dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz'
